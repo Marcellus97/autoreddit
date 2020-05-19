5ba17a283dae768b52398b892adf340f2f18a459
Marcellus97 <marcellusm.mohanna@gmail.com>
2020-05-18 20:48:40 -0400

    Better postgrabber

--- postgrabber.py
+++ postgrabber.py
@@ -13,38 +13,41 @@ PASSWORD = secret_file.readline().strip()
 secret_file.close()
 
 reddit = praw.Reddit(client_id=ID,
-		client_secret=SECRET,
-		user_agent=USERAGENT,
-		username=USERNAME,
-		password=PASSWORD)
+	    client_secret=SECRET,
+	    user_agent=USERAGENT,
+	    username=USERNAME,
+	    password=PASSWORD)
 def main():
-	dir_path = datetime.datetime.now().strftime('./%m_%d_%y---%H-%M-%S/')
-	os.mkdir(dir_path)
+    if not os.path.exists('./data_files'):
+        os.mkdir('./data_files')
+    dir_path = datetime.datetime.now().strftime('./data_files/%m_%d_%y---%H-%M-%S/')
+    os.mkdir(dir_path)
+    post_num = 0
+    for sub in reddit.front.top('day', limit=20):
 
-	post_num = 0
-	for sub in reddit.front.top('day', limit=20):
+        if sub.is_self:
+            post_num += 1
+            post_path = dir_path + '/' + str(post_num) + '/'
+            os.mkdir(post_path)
 
-		if sub.is_self:
-			post_num += 1
-			post_path = dir_path + '/' + str(post_num) + '/'
-			os.mkdir(post_path)
+            posts_file = open(post_path+ 'post.txt', 'w')
+            posts_file.write(sub.selftext)
 
-			posts_file = open(post_path+ 'post.csv', 'w')
-			posts_file.write('title,')
-			posts_file.write('name,')
-			posts_file.write('text\n')
+            posts_meta_file= open(post_path+ 'post_meta.txt', 'w')
+            posts_meta_file.write('title: ' + sub.title + '\n')
+            posts_meta_file.write('author: ' + sub.author.name+ '\n')
+            posts_meta_file.write('subreddit: ' + sub.subreddit.display_name+ '\n')
+            posts_meta_file.write('score ' + str(sub.score)+ '\n')
+            posts_meta_file.write('ratio: ' + str(sub.upvote_ratio)+ '\n')
+            posts_meta_file.write('url: ' + sub.url+ '\n')
 
-			posts_file.write(sub.title + '\n')
-			posts_file.write(sub.name + '\n')
-			posts_file.write(sub.selftext + '\n')
-
-			sub.comment_limit = 20
-			sub.comments.replace_more(limit=0)
-			com_num = 0
-			for c in sub.comments:
-				com_num += 1
-				comment_file = open(post_path+ 'comment' + str(com_num) + '.txt', 'w')
-				comment_file.write(c.body)
-				comment_file.close()
-	posts_file.close()
+            sub.comment_limit = 20
+            sub.comments.replace_more(limit=0)
+            com_num = 0
+            for c in sub.comments:
+                com_num += 1
+                comment_file = open(post_path+ 'comment' + str(com_num) + '.txt', 'w')
+                comment_file.write(c.body)
+                comment_file.close()
+    posts_file.close()
 main()
